e80996956a988a404a1aafb1e6439c612cffb36b
Baruch Burstein <bmburstein@gmail.com>
2017-12-03 12:42:13 +0200

    Add shbang to stitch script

--- scripts/stitch.py
+++ scripts/stitch.py
@@ -1,3 +1,4 @@
+#! /usr/bin/python
 # This is an initial cut of a general header stitching script.
 # It is currently hard-coded to work with Clara, but that is purely
 # a path thing. The next step is the genericise this further and
